eae6c5ad8a29a59ecac55dd96aa77f91adba031c
anthony <adkowalkowski@gmail.com>
2022-02-28 11:55:19 -0600

    added text for about page and inital styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,9 @@
 const About = () => {
     return (
         <div className='about'>
-            <p>about</p>
+            <h2>Hello! I am Anthony Kowalkowski</h2>
+            <p>I am a software engineer inspired by accessible and efficient code. Utilizing a mindful approach to development, I return user-centric and reliable results. My previous work in academia informs my ability to translate complex ideas and jargon into comprehensible content.    
+</p>
         </div>
         
     )
